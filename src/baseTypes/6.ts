eb9addd6df39898971145ae83e75b241b0c269e0
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2023-09-08 16:31:08 +0200

    primary commit
    - all tasks are done;
    - need additional explanation for  generic-task 4 & 5 ;

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,16 +2,18 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
+function showMessage(message: string): void {
+  // string для аргументу виключно через неймінг, може бути будь-який тип (any)
   console.log(message);
 }
 
-function calc(num1, num2) {
+function calc(num1: number, num2: number): number {
+  // якщо не розглядати можливість додавання рядків
   return num1 + num2;
 }
 
-function customError() {
-  throw new Error('Error');
+function customError(): never {
+  throw new Error("Error");
 }
 
 export {};
